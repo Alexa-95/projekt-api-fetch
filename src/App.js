311117ab34b427095789ff10ba2c8aa20adcee4f
Aleksandra Sobaska <Aleksandra.Makowska@grupawp.pl>
2021-11-17 22:08:32 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.number = React.createRef();
+  }
+
+  state = {
+    text: "Type the year",
+    error: ""
+  }
+
+  handleDateChange = () => {
+    const value = this.number.current.value;
+    
+    console.log(value);
+    fetch(`http://numbersapi.com/${value}/year?json`)
+      .then(res => {
+        if(res.ok){
+          return res;
+        }
+        throw Error(res.statusText)
+      })
+      .then(res => res.json())
+      .then(data => this.setState({
+        text: data.text
+      }))
+      .catch(err => {
+        this.setState({
+          text: `Coś jest nie tak :( ${err}`
+        })
+      })
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <input onChange={this.handleDateChange} type="text" ref={this.number} />
+        <p>{this.state.text}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
